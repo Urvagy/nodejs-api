3c5dd4f25b0af1a440caeb425c38bc3d9ecc3821
Veszelovszki Richárd <93860283+Urvagy@users.noreply.github.com>
2023-04-26 10:54:47 +0200

    Add files via upload

---
+++ db.js
@@ -0,0 +1,30 @@
+const sqlite3 = require("sqlite3").verbose();
+const fs = require("fs");
+
+fs.open("employee.db", "w", function (err, file) {
+  if (err) throw err;
+  console.log("Saved!");
+});
+
+const db = new sqlite3.Database(
+  "./employee.db",
+  sqlite3.OPEN_READWRITE,
+  (err) => {
+    if (err) return console.error(err.message);
+
+    console.log("connection succesful");
+  }
+);
+
+db.run(
+  "CREATE TABLE employees(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, suly TEXT NOT NULL, fekmax TEXT NOT NULL, gugmax TEXT NOT NULL)"
+);
+
+/*
+const sql ='INSERT INTO employees(name, email, address) VALUES(?,?,?)';
+db.run(sql, ["mike","asd@gmail.com", "aaaaaa aaaa"], (err)=>{
+    if(err) return console.error(err.message);
+
+    console.log("A new row added!");
+});
+*/
